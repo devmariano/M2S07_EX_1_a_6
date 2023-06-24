f7f931d75970cacef0b30c9439238eeb4e042f97
Alexandre Mariano <alexandre_mariano1@estudante.sesisenai.org.br>
2023-06-23 22:17:33 -0300

    [M2S07]Ex 5 - Editar usuário

--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -1,20 +1,90 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-const CardComponent = ({ user }) => {
-const [likes, setLikes] = useState(0);
+const CardComponent = ({ user, onSave }) => {
+  const [likes, setLikes] = useState(0);
+  const [editing, setEditing] = useState(false);
+  const [editedNickname, setEditedNickname] = useState(user.nickname);
+  const [editedIdade, setEditedIdade] = useState(user.idade);
+  const [editedEmail, setEditedEmail] = useState(user.email);
+  const [editedSenha, setEditedSenha] = useState(user.senha);
 
-const handleLike = () => {
+  const handleLike = () => {
     setLikes(likes + 1);
-};
+  };
+
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleSave = () => {
+    const editedUser = {
+      ...user,
+      nickname: editedNickname,
+      idade: editedIdade,
+      email: editedEmail,
+      senha: editedSenha,
+    };
+
+    onSave(editedUser); // Chamar a função onSave passando o usuário editado
+
+    setEditing(false);
+  };
+
   return (
-<div className="card text-white bg-secondary mb-2" style={{ maxWidth: '18rem' }}>
-      <div class="card-header"><h5 className="card-title">{user.nickname}</h5></div>
+    <div className="card text-white bg-secondary mb-2" style={{ maxWidth: '18rem' }}>
+      <div className="card-header">
+        <h5 className="card-title">
+          {editing ? (
+            <input
+              type="text"
+              value={editedNickname}
+              onChange={(e) => setEditedNickname(e.target.value)}
+            />
+          ) : (
+            user.nickname
+          )}
+        </h5>
+      </div>
       <div className="card-body">
-        <p className="card-text">Idade: {user.idade}</p>
-        <p className="card-text">Email: {user.email}</p>
-        <p className="card-text">Senha: {user.senha}</p>
-        <button className="btn btn-success" onClick={handleLike}>Dar um like: {likes}</button>
+        <p className="card-text">
+          {editing ? (
+            <input
+              type="number"
+              value={editedIdade}
+              onChange={(e) => setEditedIdade(e.target.value)}
+            />
+          ) : (
+            `Idade: ${user.idade}`
+          )}
+        </p>
+        <p className="card-text">
+          {editing ? (
+            <input
+              type="email"
+              value={editedEmail}
+              onChange={(e) => setEditedEmail(e.target.value)}
+            />
+          ) : (
+            `Email: ${user.email}`
+          )}
+        </p>
+        <p className="card-text">
+          {editing ? (
+            <input
+              type="password"
+              value={editedSenha}
+              onChange={(e) => setEditedSenha(e.target.value)}
+            />
+          ) : (
+            `Senha: ${user.senha}`
+          )}
+        </p>
+        <button className="btn btn-success" onClick={handleLike}>
+          Dar um like: {likes}
+        </button>
+        <button className="btn btn-outline-warning m-2" onClick={editing ? handleSave : handleEdit}>
+          {editing ? 'Salvar' : 'Editar'}
+        </button>
       </div>
     </div>
   );
--- src/components/FormComponent/FormComponent.jsx
+++ src/components/FormComponent/FormComponent.jsx
@@ -14,14 +14,14 @@ const FormComponent = () => {
 
     if (nickname && idade && email && senha) {
       const newUser = {
+        id: Date.now(), // Gerar um identificador único para cada usuário
         nickname,
         idade,
         email,
         senha,
       };
-      
+
       setUsers([...users, newUser]);
-      console.log(users);
       // Limpar os campos após a submissão bem-sucedida
       setNickname('');
       setIdade('');
@@ -33,48 +33,76 @@ const FormComponent = () => {
     }
   };
 
+  const handleSave = (editedUser) => {
+    setUsers((prevUsers) =>
+      prevUsers.map((user) => (user.id === editedUser.id ? editedUser : user))
+    );
+  };
+
   return (
     <div className="container">
-    <div className="row">
-      <div className="col-md-6">
-        <div className="mb-3 mt-5">
-          <h3>Cadastre um novo usuário</h3>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label className="form-label">Nickname:</label>
-            <input type="text" className="form-control" value={nickname} onChange={(e) => setNickname(e.target.value)} />
+      <div className="row">
+        <div className="col-md-6">
+          <div className="mb-3 mt-5">
+            <h3>Cadastre um novo usuário</h3>
           </div>
-          <div className="mb-3">
-            <label className="form-label">Idade:</label>
-            <input type="number" className="form-control" value={idade} onChange={(e) => setIdade(e.target.value)} />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Email:</label>
-            <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </div>
-          <div className="mb-3">
-            <label className="form-label">Senha:</label>
-            <input type="password" className="form-control" value={senha} onChange={(e) => setSenha(e.target.value)} />
-          </div>
-          <button type="submit" className="btn btn-primary">Cadastrar</button>
-        </form>
-        {errorMessage && <p>{errorMessage}</p>}
-      </div>
-      <div className="col-md-6">
-        <div className="row ">
-        <div className="mb-3 mt-5">
-          <h3>Cards dos usuários</h3>
-        </div>
-          {users.map((user, index) => (
-            <div className="col-md-6 mb-4" key={index}>
-              <CardComponent user={user} />
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label className="form-label">Nickname:</label>
+              <input
+                type="text"
+                className="form-control"
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+              />
             </div>
-          ))}
+            <div className="mb-3">
+              <label className="form-label">Idade:</label>
+              <input
+                type="number"
+                className="form-control"
+                value={idade}
+                onChange={(e) => setIdade(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Email:</label>
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Senha:</label>
+              <input
+                type="password"
+                className="form-control"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Cadastrar
+            </button>
+          </form>
+          {errorMessage && <p>{errorMessage}</p>}
+        </div>
+        <div className="col-md-6">
+          <div className="row">
+            <div className="mb-3 mt-5">
+              <h3>Cards dos usuários</h3>
+            </div>
+            {users.map((user) => (
+              <div className="col-md-6 mb-4" key={user.id}>
+                <CardComponent user={user} onSave={handleSave} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
